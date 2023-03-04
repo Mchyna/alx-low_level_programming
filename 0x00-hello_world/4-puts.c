9e42ad76c37d7e7afbd955eed05e417efb26256a
Mchyna <phumelelandeya19@gmail.com>
2023-03-04 15:36:39 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#inlude <stdio.h>
+/**
+ * main - A program that prints a sentence using put function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
