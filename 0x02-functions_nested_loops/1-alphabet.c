7b99d100ad54d4a872efae20a3e2f1a533192b9f
Mchyna <phumelelandeya19@gmail.com>
2023-03-05 03:06:13 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: None
+ */
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar (letter);
+	}
+	_putchar (10);
+}
