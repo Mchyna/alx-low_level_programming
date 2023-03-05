7a08a7d7e8aab7783f7514f052a808bcb24f52ce
Mchyna <phumelelandeya19@gmail.com>
2023-03-05 03:09:17 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
 
 void print_alphabet(void)
 {
-	int letter;
+int letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar (letter);
-	}
-	_putchar (10);
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar (10);
 }
