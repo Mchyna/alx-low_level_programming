6ef968fa8aaac4d9b06a714060df80493221b12e
Mchyna <phumelelandeya19@gmail.com>
2023-03-06 11:22:25 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,6 @@
  * @s:segment targeted
  * @accept:reference bytes container
  * Return:returns the number of bytes in the initial
- * segment of s which consist only of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
